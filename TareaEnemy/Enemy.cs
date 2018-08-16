a4126e5bc466a277b0ec16b594609b1028746770
CornnFlaek <42181325+CornnFlaek@users.noreply.github.com>
2018-08-15 21:19:56 -0700

    Add files via upload

---
+++ TareaEnemy/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaEnemigo
+{
+    class Enemy
+    {
+        public int PosicionX;
+        public int PosicionY;
+
+
+        private int _habbocreditos;
+        public int Robux
+        {
+            get
+            {
+                return _habbocreditos;
+            }
+            set
+            {
+                if (_habbocreditos < 100)
+                {
+                    _habbocreditos = 100;
+                }
+            }
+        }
+
+
+        public void Mover()
+        {
+
+        }
+
+        public void Disparar()
+        {
+
+        }
+
+        public void Morir()
+        {
+
+        }
+    }
+}
